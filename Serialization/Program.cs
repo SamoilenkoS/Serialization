5b5ce0b7c41befcf0a8b66b8bb8e5699faa71227
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-06-22 19:13:09 +0300

    V5 Commit

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -4,30 +4,37 @@ using System.Xml.Serialization;
 
 namespace Serialization
 {
-    class Program//V3
+    class Program//V5
     {
         static void Main()
         {
-            var student = new Student();
-            using (var streamReader = new StreamReader("Students.txt"))
-            {
-                student.FirstName = streamReader.ReadLine();
-                student.LastName = streamReader.ReadLine();
-            }
-
-            SerializeXML(student);
+            var student = DeserializeXml();
 
             Console.WriteLine($"{student}");
             Console.ReadKey();
         }
 
-        private static void SerializeXML(Student student)
+        private static void SerializeXml(Student student)
         {
             var serializer = new XmlSerializer(typeof(Student));
-            using (var streamWritter = new StreamWriter("Students.txt"))
+            using (var streamWriter = new StreamWriter("Students.txt"))
             {
-                serializer.Serialize(streamWritter, student);
+                serializer.Serialize(streamWriter, student);
             }
+        }
+
+        private static Student DeserializeXml()
+        {
+            var serializer = new XmlSerializer(typeof(Student));
+            using (var streamReader = new StreamReader("Students.txt"))
+            {
+                if (serializer.Deserialize(streamReader) is Student result)
+                {
+                    return result;
+                }
+            }
+
+            return null;
         }
     }
 }
