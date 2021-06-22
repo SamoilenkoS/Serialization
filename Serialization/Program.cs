59ee76d6085717ea71a78316367198eb5a68b61f
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-06-22 19:00:51 +0300

    V3 Commit

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -3,20 +3,16 @@ using System.IO;
 
 namespace Serialization
 {
-    class Program//V2
+    class Program//V3
     {
         static void Main()
         {
             var student = new Student();
-            Console.Write("Enter name of student:");
-            student.FirstName = Console.ReadLine();
-
-            Console.Write("Enter last name of student:");
-            student.LastName = Console.ReadLine();
-
-            var streamWriter = new StreamWriter("Students.txt");
-            streamWriter.WriteLine($"{student.FirstName}{Environment.NewLine}{student.LastName}{Environment.NewLine}");
-            streamWriter.Close();
+            using (var streamReader = new StreamReader("Students.txt"))
+            {
+                student.FirstName = streamReader.ReadLine();
+                student.LastName = streamReader.ReadLine();
+            }
 
             Console.WriteLine($"{student}");
             Console.ReadKey();
