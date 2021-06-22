aacd66446193ea68ed3f2c12e22917ed9764565d
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-06-22 18:45:43 +0300

    Add project files.

---
+++ Serialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main()
+        {
+            var student = new Student();
+            Console.Write("Enter name of student:");
+            student.FirstName = Console.ReadLine();
+
+            Console.Write("Enter last name of student:");
+            student.LastName = Console.ReadLine();
+
+            Console.WriteLine($"{student}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Serialization/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Serialization
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+            => $"{nameof(FirstName)}={FirstName}, {nameof(LastName)}={LastName}";
+    }
+}
